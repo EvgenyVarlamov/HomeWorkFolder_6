ba52b1962e80db9ac8fa73ca97d5cb03a4c300ba
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-12 23:28:16 +0300

    add Additional Homework #2 Endless cycle

---
+++ Homework_005_Additional/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Есть программа с бесконечным циклом. 
+Когда пользователь вводит exit программа заканчивается */
+
+// Первый вариант
+for(;;)
+{
+    Console.WriteLine("Вы в бесконечном цикле 1");
+    string? command = Console.ReadLine();
+    if(command?.ToLower() == "exit")
+    {
+        break;
+    } 
+    Console.WriteLine("...");
+}
+/////////////////////////////////////////////////////////////////
+
+// Второй вариант
+while(true)
+{ Console.WriteLine("Вы в бесконечном цикле 2");
+    string? command = Console.ReadLine();
+    if(command?.ToLower() != "exit")
+    {
+        Console.WriteLine("...");
+    } 
+    else
+    {
+        break;
+    } 
+}
+/////////////////////////////////////////////////////////////////
+
+// Третий вариант
+while(EndlessCycleStop())
+{
+    Console.WriteLine("Вы в бесконечном цикле 3");
+}
+
+bool EndlessCycleStop()
+{
+    bool result = true;
+    string? command = Console.ReadLine();
+    if(command?.ToLower() == "exit")
+    {
+        result = false;
+    }     
+    return result;
+}
